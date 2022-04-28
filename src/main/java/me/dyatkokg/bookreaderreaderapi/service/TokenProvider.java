c019cb6230f97f128b21f80db4e39ab5b989a020
dyatkokg <dyatkokg@gmail.com>
2022-04-28 16:38:36 +0400

    add token signature at cloud config

--- src/main/java/me/dyatkokg/bookreaderreaderapi/service/TokenProvider.java
+++ src/main/java/me/dyatkokg/bookreaderreaderapi/service/TokenProvider.java
@@ -4,16 +4,18 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class TokenProvider {
 
-    private static final String SIGNATURE = "x7HktL7QmQwR7Ii98AU3eACZ70arilFhCIgz54IYEOMCL0UIl7MWCBXeEzyQ6syQmWXWJACQzOWkrWd2DBnibCbENjd6fV7m073e27KkERwBSrmSX0hv28P8rP6LSXY1xs24Fr6w3Se7amJ9fRSPfIL0cXdujrtUUPjPp8M1M6SKJezzOMkSKZtihTKRscQrBjcTPJWs";
+    @Value("${token.signature}")
+    private String signature;
 
     public void validateToken(String token) {
-        DecodedJWT verified = JWT.require(Algorithm.HMAC256(SIGNATURE))
+        DecodedJWT verified = JWT.require(Algorithm.HMAC256(signature))
                 .withIssuer("book-app").build().verify(token);
     }
 }
